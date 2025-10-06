f84bd14d296de2130931c0a980993db631912c31
pierpaolo <tach@hiho.it>
2025-10-06 12:23:58 +0200

    passato a prod

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@
  */
 
 // Environment configuration
-var USE_PROD = false;
+var USE_PROD = true;
 var API_BASE_URL_DEV = 'https://www.ercolaniapps-dev.hiho.it';
 var API_BASE_URL_PROD = 'https://www.ercolaniapps.hiho.it';
 var API_BASE_URL = USE_PROD ? API_BASE_URL_PROD : API_BASE_URL_DEV;
--- config/environment.php
+++ config/environment.php
@@ -5,7 +5,7 @@
  */
 
 // Environment configuration
-$USE_PROD = false;
+$USE_PROD = true;
 $API_BASE_URL_DEV = 'https://ercolaniapps-dev.hiho.it';  // Removed www to avoid 301 redirect
 $API_BASE_URL_PROD = 'https://www.ercolaniapps.hiho.it';
 $API_BASE_URL = $USE_PROD ? $API_BASE_URL_PROD : $API_BASE_URL_DEV;
